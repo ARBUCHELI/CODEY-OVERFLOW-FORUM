01316882ebd055935789c6c9e526f44be1ff54e2
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-08 17:52:27 +0300

    refactoring the app

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Header = (props) => {
   return (
     <>
-      <img src={props.profileImg} alt="profile"></img>
+      <img src={props.profileImg} alt=""></img>
       <h1>{props.username}</h1>
     </>
-  )
+  );
 }
 
 export default Header;
