c82c17ca137e2d40e5f304484a9e7b5ac48ea468
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-09 15:09:58 +0300

    refactoring the app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const App = () => {
   return (
-    <div>
+    <div style={{border: "1px solid transparent", width: "50%", marginLeft: "28%", padding: "1em"}}>
       {
         comments.map(comment => 
           <Card commentObject={comment} />
